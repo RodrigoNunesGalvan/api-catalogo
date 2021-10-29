05fcce5af516cf4d19a4e9b429456f8c92613abb
Rodrigo Nunes <rodrigongalvan@hotmail.com>
2021-10-29 09:17:11 -0300

    Primeiro Commit API catálogo de produtos

---
+++ src/test/java/br/com/catalogo/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.catalogo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
